0c57b4162013d6c14b5cb488520661f0889d39e7
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2024-01-02 22:22:34 +0100

    1. Utworzenie contacts.json, package.json i index,js

---
+++ index.js
@@ -0,0 +1,6 @@
+const fs = require("fs").promises;
+const { program } = require("commander");
+const contacts = require("./contacts");
+
+console.log("test test");
+console.log(contacts.removeContact());
